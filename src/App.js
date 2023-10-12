160e10c1554cbcfed7fc2ad7e5fba1bb50e58c26
Yug <03yugagarwal@gmail.com>
2023-10-12 09:37:03 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Task</th>
+          <th>Repository Link</th>
+          <th>Live</th>
+        </tr>
+        <tr>
+          <td>Task 1</td>
+          <td><a href="https://github.com/03YugAgarwal/task1-ethnus" target='_blank'>Github</a></td>
+          <td><a href="https://task1-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 2</td>
+          <td><a href="https://github.com/03YugAgarwal/task2-ethnus" target='_blank'>Github</a></td>
+          <td><a href="https://task2-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 3</td>
+          <td><a href="https://github.com/03YugAgarwal/task3-ethnus" target='_blank'>Github</a></td>
+          <td><a href="https://task3-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 4</td>
+          <td><a href="https://github.com/03YugAgarwal/task4-ethnus" target='_blank'>Github</a></td>
+          <td><a href="https://task4-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 5</td>
+          <td><a href="https://github.com/03YugAgarwal/task5-ethnus" target='_blank'>Github</a></td>
+          <td><a href="https://task5-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 6</td>
+          <td><a href="https://github.com/03YugAgarwal/portfoliov2" target='_blank'>Github</a></td>
+          <td><a href="https://yugagarwal.codes/" target='_blank' >Live</a></td>
+        </tr>
+      </table>
     </div>
   );
 }
